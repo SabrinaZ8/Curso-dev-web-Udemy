f00b4fa8aaa47e5bfb378750bd8638d723a85748
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-09-23 17:58:52 -0300

    Desafio Lista de frutas secao 16: projeto 15

--- listFruits/script.js
+++ listFruits/script.js
@@ -1,1 +1,10 @@
+let fruitLIst = document.getElementById('fruitList')
+
+const fruits = ['Melancia', 'Ameixa', 'Morango', 'Laranja', 'Uva']
+
+for(let i = 0; i < fruits.length; i++) {
+    let listItem = document.createElement('li')
+    listItem.textContent = fruits[i]
+    fruitLIst.appendChild(listItem)
+}
 
