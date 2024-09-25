882f26279ead28e49e30147396b64c2348b7533d
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-09-25 10:32:52 -0300

    Adicionando minha versao do contador de letras do projeto 37

--- counterLetters/my-version/main.js
+++ counterLetters/my-version/main.js
@@ -1,3 +1,9 @@
 const textInput = document.getElementById('text-input');
 const countButton = document.getElementById('count-button');
 const result = document.getElementById('result');
+
+countButton.addEventListener('click', function() {
+    let removeEspacos = textInput.value.split(" ").join("")
+    const countLetters = removeEspacos.length
+    result.innerText = `The text contains ${countLetters} letters.`
+})
